a9bb4def91bedb423f33c623c64cf7c45980630f
rajes <rajesh-at-196071714745>
2024-12-11 02:47:09 +0530

    about us page changes

--- src/app/about-us/about-us.component.ts
+++ src/app/about-us/about-us.component.ts
@@ -1,13 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { SharedModule } from '../shared/shared.module';
+import { CarouselModule } from 'ngx-bootstrap/carousel';
 
 @Component({
   selector: 'app-about-us',
   standalone: true,
-  imports: [SharedModule],
+  imports: [SharedModule,CarouselModule],
   templateUrl: './about-us.component.html',
   styleUrl: './about-us.component.scss'
 })
 export class AboutUsComponent {
-
+  itemsPerSlide = 7;
+   constructor(){
+    this.updateItemsPerSlide();
+   }
+   ngOnInit(){
+    this.updateItemsPerSlide();
+   }
+   @HostListener('window:resize', ['$event'])
+   onResize(event: any): void{
+    this.updateItemsPerSlide();
+   }
+   updateItemsPerSlide():void{
+    this.itemsPerSlide = window.innerWidth <768 ? 1 : 7;
+   }
+  singleSlideOffset = true;
+  noWrap = true;
+  slides = [
+    {image: 'assets/img/client_logos/dell.png'},
+    {image: 'assets/img/client_logos/zendesk.png'},
+    {image: 'assets/img/client_logos/rakuten.png'},
+    {image: 'assets/img/client_logos/pacific.png'},
+    {image: 'assets/img/client_logos/ncr.png'},
+    {image: 'assets/img/client_logos/lattice.png'},
+    {image: 'assets/img/client_logos/ted.png'}
+  ];
 }
